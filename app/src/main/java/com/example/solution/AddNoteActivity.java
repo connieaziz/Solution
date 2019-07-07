560d500625db82be85ee33c87c7954f0277664f5
connieaziz <connieaziz@gmail.com>
2019-07-07 15:06:16 +0300

    file upload

--- app/src/main/java/com/example/solution/AddNoteActivity.java
+++ app/src/main/java/com/example/solution/AddNoteActivity.java
@@ -1,16 +1,17 @@
 package com.example.solution;
 
-import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.ImageView;
 
 import com.example.solution.database.DatabaseHelper;
 import com.example.solution.database.Note;
@@ -24,6 +25,8 @@ public class AddNoteActivity extends AppCompatActivity {
     Button btnSave;
     String noteText;
     String title;
+    ImageView imgView;
+    private static final int CAPTURE_IMAGE_REQUEST_CODE=500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +41,15 @@ public class AddNoteActivity extends AppCompatActivity {
         btnAddPhoto = findViewById(R.id.btnAddPhoto);
         btnAddVoiceNote = findViewById(R.id.btnAddVoiceNote);
         btnSave = findViewById(R.id.btnSave);
+       imgView = findViewById(R.id.img);
 
-
+        btnAddPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,CAPTURE_IMAGE_REQUEST_CODE);
+            }
+        });
 
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,7 +63,15 @@ public class AddNoteActivity extends AppCompatActivity {
                 Log.d("AddNote","The number of notes is "+rows);
             }
         });
-
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==CAPTURE_IMAGE_REQUEST_CODE && requestCode==RESULT_OK){
+            Bundle bundle = data.getExtras();
+            Bitmap bitmap=(Bitmap)bundle.get("data");
+            imgView.setImageBitmap(bitmap);
+        }
+    }
 }
